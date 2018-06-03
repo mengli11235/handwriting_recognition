83ff45b69fe2761ddf01d01b9f46ac8557af3a44
atianhlu <31670047+atianhlu@users.noreply.github.com>
2018-06-03 19:45:01 +0200

    Update confusion_matrix_v0.py

--- confusion_matrix_v0.py
+++ confusion_matrix_v0.py
@@ -10,28 +10,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # 
 # # Confusion matrix
 # 
-# 
-# Example of confusion matrix usage to evaluate the quality
-# of the output of a classifier on the iris data set. The
-# diagonal elements represent the number of points for which
-# the predicted label is equal to the true label, while
-# off-diagonal elements are those that are mislabeled by the
-# classifier. The higher the diagonal values of the confusion
-# matrix the better, indicating many correct predictions.
-# 
-# The figures show the confusion matrix with and without
-# normalization by class support size (number of elements
-# in each class). This kind of normalization can be
-# interesting in case of class imbalance to have a more
-# visual interpretation of which class is being misclassified.
-# 
-# Here the results are not as good as they could be as our
-# choice for the regularization parameter C was not the best.
-# In real life applications this parameter is usually chosen
-# using `grid_search`.
-# 
-# 
-# 
+
 
 # In[24]:
 
