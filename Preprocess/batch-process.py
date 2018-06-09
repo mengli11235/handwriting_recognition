ac1e725f9808ed22c737bbc2c9798158a5e7cff4
Josleee <cz2f@qq.com>
2018-06-09 21:26:54 +0200

    Update

--- Preprocess/batch-process.py
+++ Preprocess/batch-process.py
@@ -4,7 +4,7 @@ import os
 import cv2
 import numpy as np
 from skimage import img_as_ubyte
-from skimage.filters import (threshold_sauvola)
+from skimage.filters import (threshold_sauvola, threshold_otsu)
 
 # set your working directory
 os.chdir('../')
@@ -14,24 +14,37 @@ for d in dirList:
     image = cv2.imread(d)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    window_size = 75
+    window_size = 15
     thresh_sauvola = threshold_sauvola(image, window_size=window_size, k=0.45)
     binary_sauvola = image > thresh_sauvola
+    binary_global = image > threshold_otsu(image)
 
-    cv_image = img_as_ubyte(binary_sauvola)
+    cv_image = img_as_ubyte(binary_global)
     cv_image = cv2.GaussianBlur(cv_image, (5, 5), 0)
     # cv2.imwrite(os.path.join('./Output/sauvola/', d.split('/')[-1]), cv_image)
 
-    ret, labels = cv2.connectedComponents(cv_image)
-    # Map component labels to hue val
-    label_hue = np.uint8(179 * labels / np.max(labels))
-    blank_ch = 255 * np.ones_like(label_hue)
-    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
-    # cvt to BGR for display
-    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-    # set bg label to black
-    labeled_img[label_hue == 0] = 0
-    # cv2.imshow('labeled.png', labeled_img)
-    cv2.imwrite(os.path.join('./Output/connected/', d.split('/')[-1]), labeled_img)
+    # ret, labels = cv2.connectedComponents(cv_image)
+    # # Map component labels to hue val
+    # label_hue = np.uint8(179 * labels / np.max(labels))
+    # blank_ch = 255 * np.ones_like(label_hue)
+    # labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+    # # cvt to BGR for display
+    # labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+    # # set bg label to black
+    # labeled_img[label_hue == 0] = 0
+    # # cv2.imshow('labeled.png', labeled_img)
+    # cv2.imwrite(os.path.join('./Output/connected/', d.split('/')[-1]), labeled_img)
+
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(cv_image, connectivity=4)
+    sizes = stats[:, -1]
+    max_label = 1
+    max_size = sizes[1]
+    for i in range(2, nb_components):
+        if sizes[i] > max_size:
+            max_label = i
+            max_size = sizes[i]
+    img2 = np.zeros(output.shape)
+    img2[output == max_label] = 255
+    cv2.imwrite(os.path.join('./Output/connected2/', d.split('/')[-1]), img2)
 
     print("success")
--- Preprocess/connected-component.py
+++ Preprocess/connected-component.py
@@ -19,12 +19,23 @@ thresh_sauvola = threshold_sauvola(image, window_size=window_size, k=0.9)
 binary_sauvola = image > thresh_sauvola
 binary_global = image > threshold_otsu(image)
 
-cv_image = img_as_ubyte(binary_sauvola)
+cv_image = img_as_ubyte(binary_global)
 ret, labels = cv2.connectedComponents(cv_image)
 
+nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(cv_image, connectivity=8)
+sizes = stats[:, -1]
+max_label = 1
+max_size = sizes[1]
+for i in range(2, nb_components):
+    if sizes[i] > max_size:
+        max_label = i
+        max_size = sizes[i]
+img2 = np.zeros(output.shape)
+img2[output == max_label] = 255
+
 # cv_image = (255 - cv_image)
-plt.imshow(cv_image, cmap=plt.cm.gray)
-plt.show()
+# plt.imshow(cv_image, cmap=plt.cm.gray)
+# plt.show()
 
 # Map component labels to hue val
 label_hue = np.uint8(179 * labels / np.max(labels))
@@ -37,6 +48,17 @@ labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
 # set bg label to black
 labeled_img[label_hue == 0] = 0
 
+nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(cv_image, connectivity=4)
+sizes = stats[:, -1]
+max_label = 1
+max_size = sizes[1]
+for i in range(2, nb_components):
+    if sizes[i] > max_size:
+        max_label = i
+        max_size = sizes[i]
+img2 = np.zeros(output.shape)
+img2[output == max_label] = 255
+
 # cv2.imshow('labeled.png', labeled_img)
-plt.imshow(labeled_img, cmap=plt.cm.gray)
+plt.imshow(img2, cmap=plt.cm.gray)
 plt.show()
