6f30cd61a0893842930cde1ada193ec447192bf8
Josleee <cz2f@qq.com>
2018-06-11 19:25:58 +0200

    Add rotate module

--- Preprocess/histogram.py
+++ Preprocess/histogram.py
@@ -1,3 +1,5 @@
+import math
+
 import cv2
 import matplotlib
 import matplotlib.pyplot as plt
@@ -81,7 +83,7 @@ def threshold_li(image):
 
 
 # image = cv2.imread('../Labels/seg_test.jpg')
-image = cv2.imread('../Labels/P123-Fg001-R-C01-R01-fused.jpg')
+image = cv2.imread('../Labels/P564-Fg003-R-C01-R01-fused.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 window_size = 29
@@ -176,35 +178,102 @@ plt.imshow(rotated, cmap=plt.cm.gray)
 plt.show()
 
 
-def separate_words(line):
-    line_hist = cv2.reduce(line, 0, cv2.REDUCE_AVG).reshape(-1)
-    new_line = cv2.cvtColor(line, cv2.COLOR_GRAY2BGR)
-    line_peaks = peakdetect(line_hist, lookahead=80)
-    Hl, Wl = new_line.shape[:2]
-
-    for y in line_peaks[0]:
-        plt.plot(y[0], y[1], "r*")
-        cv2.line(new_line, (y[0], 0), (y[0], Hl), (255, 0, 0), 3)
-    for y in line_peaks[1]:
-        plt.plot(y[0], y[1], "g*")
-        cv2.line(new_line, (y[0], 0), (y[0], Hl), (0, 255, 0), 3)
-        # print(y)
-    plt.plot(line_hist)
-    plt.show()
-    return new_line
+def rotateImage(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
 
 
-y0 = 0
-for y in peaks[0]:
-    crop_img = s_img_2[y0:y[0], 0:W]
-    y0 = y[0]
-    crop_img = separate_words(crop_img)
-    plt.imshow(crop_img, cmap=plt.cm.gray)
-    plt.show()
+def rotatedRectWithMaxArea(w, h, angle):
+    """
+    Given a rectangle of size wxh that has been rotated by 'angle' (in
+    radians), computes the width and height of the largest possible
+    axis-aligned rectangle (maximal area) within the rotated rectangle.
+    """
+    if w <= 0 or h <= 0:
+        return 0, 0
 
-    if y[0] == peaks[0][-1][0]:
-        crop_img = s_img_2[y[0]:H, 0:W]
-        crop_img = separate_words(crop_img)
-        plt.imshow(crop_img, cmap=plt.cm.gray)
-        plt.show()
-        # print('last')
+    width_is_longer = w >= h
+    side_long, side_short = (w, h) if width_is_longer else (h, w)
+
+    # since the solutions for angle, -angle and 180-angle are all the same,
+    # if suffices to look at the first quadrant and the absolute values of sin,cos:
+    sin_a, cos_a = abs(math.sin(angle)), abs(math.cos(angle))
+    if side_short <= 2. * sin_a * cos_a * side_long or abs(sin_a - cos_a) < 1e-10:
+        # half constrained case: two crop corners touch the longer side,
+        #   the other two corners are on the mid-line parallel to the longer line
+        x = 0.5 * side_short
+        wr, hr = (x / sin_a, x / cos_a) if width_is_longer else (x / cos_a, x / sin_a)
+    else:
+        # fully constrained case: crop touches all 4 sides
+        cos_2a = cos_a * cos_a - sin_a * sin_a
+        wr, hr = (w * cos_a - h * sin_a) / cos_2a, (h * cos_a - w * sin_a) / cos_2a
+
+    return wr, hr
+
+
+def rotate_bound(image, angle):
+    # CREDIT: https://www.pyimagesearch.com/2017/01/02/rotate-images-correctly-with-opencv-and-python/
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+    return cv2.warpAffine(image, M, (nW, nH))
+
+
+def rotate_max_area(image, angle):
+    """ image: cv2 image matrix object
+        angle: in degree
+    """
+    wr, hr = rotatedRectWithMaxArea(image.shape[1], image.shape[0], math.radians(angle))
+    rotated = rotate_bound(image, angle)
+    h, w, _ = rotated.shape
+    y1 = h // 2 - int(hr / 2)
+    y2 = y1 + int(hr)
+    x1 = w // 2 - int(wr / 2)
+    x2 = x1 + int(wr)
+    return rotated[y1:y2, x1:x2]
+
+
+rotated = rotate_max_area(rotated, -5)
+plt.imshow(rotated, cmap=plt.cm.gray)
+plt.show()
+
+# def separate_words(line):
+#     line_hist = cv2.reduce(line, 0, cv2.REDUCE_AVG).reshape(-1)
+#     new_line = cv2.cvtColor(line, cv2.COLOR_GRAY2BGR)
+#     line_peaks = peakdetect(line_hist, lookahead=80)
+#     Hl, Wl = new_line.shape[:2]
+#
+#     for y in line_peaks[0]:
+#         plt.plot(y[0], y[1], "r*")
+#         cv2.line(new_line, (y[0], 0), (y[0], Hl), (255, 0, 0), 3)
+#     for y in line_peaks[1]:
+#         plt.plot(y[0], y[1], "g*")
+#         cv2.line(new_line, (y[0], 0), (y[0], Hl), (0, 255, 0), 3)
+#         # print(y)
+#     plt.plot(line_hist)
+#     plt.show()
+#     return new_line
+#
+#
+# y0 = 0
+# for y in peaks[0]:
+#     crop_img = s_img_2[y0:y[0], 0:W]
+#     y0 = y[0]
+#     crop_img = separate_words(crop_img)
+#     plt.imshow(crop_img, cmap=plt.cm.gray)
+#     plt.show()
+#
+#     if y[0] == peaks[0][-1][0]:
+#         crop_img = s_img_2[y[0]:H, 0:W]
+#         crop_img = separate_words(crop_img)
+#         plt.imshow(crop_img, cmap=plt.cm.gray)
+#         plt.show()
+#         # print('last')
