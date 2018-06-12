bd21f96b2f73eeec5fb90ec89d0fd97aff884b95
Josleee <cz2f@qq.com>
2018-06-12 21:44:33 +0200

    Refine generator

--- Yolo_related/autonomous_labelling.py
+++ Yolo_related/autonomous_labelling.py
@@ -7,72 +7,75 @@ from lxml import etree
 # set your working directory
 os.chdir('../../../')
 goal_dir = './Others/Annotation/'
+source_dir = 'Others/monkbrill_171005/'
 
-c_name = 'Alef'
-dirList = glob.glob("Others/monkbrill_171005/" + c_name + "/*.pgm")
-for d in dirList:
-    image = cv2.imread(d)
-    H, W = image.shape[:2]
+for f in [x[0] for x in os.walk(source_dir)]:
+    c_name = f.split('/')[-1]
+    print('Start process ' + c_name)
 
-    # create XML
-    root = etree.Element('annotation')
+    dirList = glob.glob(f + "/*.pgm")
+    for d in dirList:
+        image = cv2.imread(d)
+        H, W = image.shape[:2]
 
-    # another child with text
-    folder = etree.Element('folder')
-    folder.text = 'characters'
-    root.append(folder)
+        # create XML
+        root = etree.Element('annotation')
 
-    filename = etree.Element('filename')
-    filename.text = d.split('/')[-1]
-    root.append(filename)
+        # another child with text
+        folder = etree.Element('folder')
+        folder.text = 'characters'
+        root.append(folder)
 
-    path = etree.Element('path')
-    path.text = '/' + d
-    root.append(path)
+        filename = etree.Element('filename')
+        filename.text = d.split('/')[-1]
+        root.append(filename)
 
-    source = etree.Element('source')
-    database = etree.SubElement(source, "database")
-    database.text = 'Unknown'
-    root.append(source)
+        path = etree.Element('path')
+        path.text = '/' + d
+        root.append(path)
 
-    size = etree.Element('size')
-    width = etree.SubElement(size, 'width')
-    width.text = str(W)
-    height = etree.SubElement(size, 'height')
-    height.text = str(H)
-    depth = etree.SubElement(size, 'depth')
-    depth.text = '3'
-    root.append(size)
+        source = etree.Element('source')
+        database = etree.SubElement(source, "database")
+        database.text = 'Unknown'
+        root.append(source)
 
-    segmented = etree.Element('segmented')
-    segmented.text = '0'
-    root.append(segmented)
+        size = etree.Element('size')
+        width = etree.SubElement(size, 'width')
+        width.text = str(W)
+        height = etree.SubElement(size, 'height')
+        height.text = str(H)
+        depth = etree.SubElement(size, 'depth')
+        depth.text = '3'
+        root.append(size)
 
-    object = etree.Element('object')
-    name = etree.SubElement(object, 'name')
-    name.text = c_name
-    pose = etree.SubElement(object, 'pose')
-    pose.text = 'Unspecified'
-    truncated = etree.SubElement(object, 'truncated')
-    truncated.text = '0'
-    difficult = etree.SubElement(object, 'difficult')
-    difficult.text = '0'
-    root.append(object)
+        segmented = etree.Element('segmented')
+        segmented.text = '0'
+        root.append(segmented)
 
-    bndbox = etree.Element('bndbox')
-    xmin = etree.SubElement(bndbox, 'xmin')
-    xmin.text = '0'
-    ymin = etree.SubElement(bndbox, 'ymin')
-    ymin.text = '0'
-    xmax = etree.SubElement(bndbox, 'xmax')
-    xmax.text = str(W)
-    ymax = etree.SubElement(bndbox, 'ymax')
-    ymax.text = str(H)
-    object.append(bndbox)
+        object = etree.Element('object')
+        name = etree.SubElement(object, 'name')
+        name.text = c_name
+        pose = etree.SubElement(object, 'pose')
+        pose.text = 'Unspecified'
+        truncated = etree.SubElement(object, 'truncated')
+        truncated.text = '0'
+        difficult = etree.SubElement(object, 'difficult')
+        difficult.text = '0'
+        root.append(object)
 
-    # pretty string
-    # print(etree.tostring(root, pretty_print=True, encoding='unicode'))
-    with open(goal_dir + d.split('/')[-1].split('.')[0] + '.xml', 'w') as file:
-        file.write(etree.tostring(root, pretty_print=True, encoding='unicode'))
+        bndbox = etree.Element('bndbox')
+        xmin = etree.SubElement(bndbox, 'xmin')
+        xmin.text = '0'
+        ymin = etree.SubElement(bndbox, 'ymin')
+        ymin.text = '0'
+        xmax = etree.SubElement(bndbox, 'xmax')
+        xmax.text = str(W)
+        ymax = etree.SubElement(bndbox, 'ymax')
+        ymax.text = str(H)
+        object.append(bndbox)
 
-    print("success")
+        # pretty string
+        # print(etree.tostring(root, pretty_print=True, encoding='unicode'))
+        with open(goal_dir + d.split('/')[-1].split('.')[0] + '.xml', 'w') as file:
+            file.write(etree.tostring(root, pretty_print=True, encoding='unicode'))
+
