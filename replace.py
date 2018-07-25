1f967d09e5d58dafa764b686b911ad5a05894663
Josleee <cz2f@qq.com>
2018-07-25 14:48:27 +0800

    Update

--- replace.py
+++ replace.py
@@ -1,19 +1,7 @@
 # coding: utf-8
+import os
 
-
-def rreplace(self, old, new, *max):
-    count = len(self)
-    if max and str(max[0]).isdigit():
-        count = max[0]
-    return new.join(self.rsplit(old, count))
-
-
-# 这是一个逆序输出的函数，我在网上看到的，但我想了一下，因为我们现有的输出已经逆序了
-# 但是词内顺序又是顺着来的，比如alef，所以不用逆序
-# 放在这里仅供参考即可
-# https://blog.csdn.net/c465869935/article/details/71106967（详解在这里）
-
-f = open(r'./P25-Fg001.txt')
+f = open(r'./P123-Fg002-R-C01-R01-fused.txt')
 text = f.read()
 
 a = text.replace("Alef", "א")
@@ -43,7 +31,17 @@ x = w.replace("Tsadi-medial", "צ")
 y = x.replace("Waw", "ו")
 z = y.replace("Yod", "י")
 result = z.replace("Zayin", "ז")
+result = result.replace("-", "")
 
-fo = open(r'./P25-Fg001_new.txt', 'w', encoding='utf-8')
+fo = open(r'./tmp.txt', 'w', encoding='utf-8')
 fo.write(result)
 fo.close()
+
+fn = open(r'./new.txt', 'w', encoding='utf-8')
+
+for line in list(open(r'./tmp.txt')):
+    b = line[::-1]
+    fn.writelines(b.strip() + '\n')
+fo.close()
+
+os.remove('./tmp.txt')
